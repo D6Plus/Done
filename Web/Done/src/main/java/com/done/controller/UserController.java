0eff164abfe8dc499c4d1b017c3fb794e7aa6b65
XTG-L <xtg-l@foxmail.com>
2020-05-03 15:54:42 +0800

    再来一次

--- Web/Done/src/main/java/com/done/controller/UserController.java
+++ Web/Done/src/main/java/com/done/controller/UserController.java
@@ -2,12 +2,14 @@ package com.done.controller;
 
 import com.done.entity.User;
 import com.done.service.UserService;
+import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,8 +23,8 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(value = "index")
-    private String createNewPlan(Model model) {
-        userService.createNewPlan("test plan2", "this plan is for test");
+    private String createNewPlan(Model model, @RequestParam("planName") String planName, @RequestParam("planDescribe") String planDescribe) {
+        userService.createNewPlan(planName, planDescribe);
         return "index";
     }
 }
