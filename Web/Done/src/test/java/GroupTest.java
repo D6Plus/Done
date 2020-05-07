b1d2438c7278d825a5fb7b968e789838213f6770
An1ess <53031555+An1ess@users.noreply.github.com>
2020-05-07 19:40:57 +0800

    1

--- Web/Done/src/test/java/GroupTest.java
+++ Web/Done/src/test/java/GroupTest.java
@@ -17,60 +17,60 @@ public class GroupTest {
     GroupDAO groupDAO;
 
 
-    /*//小组创建
+   /* //小组创建
     @Test
     public void testInsertGroup(){
         Group group=new Group();
-        group.setGroupDescribe("草泥马2号");
-        group.setGroupName("草泥马草泥马");
+        group.setGroupDescribe("Edward Gaming");
+        group.setGroupName("国电");
         User user=new User();
         user.setUserID("ZENGgg");
         user.setPwd("xianggeniub123456");
         groupDAO.insertGroup(group);
         groupDAO.insertUserrole(group,user);
-    }
+    }*/
 
 
-    //加入小组
+  /* ///加入小组
     @Test
     public void testJoinGroup(){
         User user=new User();
         Group group=new Group();
         user.setUserID("xiangge789");
         user.setPwd("951753");
-        group.setGroupDescribe("我们是冠军");
-        group.setGroupName("RNG");
-        group.setGroupID(22);
+        group.setGroupDescribe("Edward Gaming");
+        group.setGroupName("国电");
+        group.setGroupID(95);
         System.out.println(group);
         groupDAO.joinGroup(user,group);
-    }
+    }*/
 
     //退出小组
-    @Test
+   /* @Test
     public void testquitGroup(){
         User user =new User();
         Group group=new Group();
-        user.setUserID("HGNB");
-        user.setPwd("123132");
-        group.setGroupID(5);
+        user.setUserID("xiangge789");
+        user.setPwd("951753");
+        group.setGroupID(95);
         group.setGroupName("nimasile");
         groupDAO.quitGroup(user,group);
 
     }*/
 
-    //查询所有小组
+   /*//查询所有小组
     @Test
     public void testqueryAllGroup(){
         List<Group> group=groupDAO.queryAllGroup();
         for(Group group1:group){
             System.out.println(group1);
         }
-    }
+    }*/
 
     //通过名字查询小组
     @Test
     public void testqueryGroupByName(){
-        String s="草泥马草泥马";
+        String s="国电";
         List<Group> group=groupDAO.queryGroupByName(s);
         for(Group group1:group){
             System.out.println(group1);
