9783177a7cede44d140b2bf298add19f78665ce1
XTG-L <xtg-l@foxmail.com>
2020-06-01 15:29:21 +0800

    fin

---
+++ Web/Done/donessm/src/main/web/wk/dao/UserDAO.java
@@ -0,0 +1,24 @@
+package wk.dao;
+
+import wk.entity.User;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserDAO {
+    User login(@Param("userID") String userID, @Param("pwd") String pwd);
+    void createNewUser(@Param("newUserID") String newUserID, @Param("userName") String userName,@Param("pwd") String pwd ,@Param("role") String role);
+    void changePwd(@Param("userID") String userID,@Param("newpwd") String newpwd);
+    void setRole(@Param("userID") String userID,@Param("role") String role);
+    User getMassage(@Param("userID") String userID);
+    void updateMassage(@Param("userID") String userID,@Param("userName") String userName,@Param("pNum") String pNum,
+                       @Param("userSex") String userSex,@Param("userBirth") String userBirth);
+    void changeRole(@Param("userID") String userID,@Param("newrole") String newrole);
+    User queryByID(@Param("userID") String userID);
+    List<User> queryByName(@Param("userName") String userName);
+    List<User> queryAll();
+    String getLastID();
+    void insertUser(@Param("userID") String userID, @Param("userName") String userName, @Param("pwd") String pwd, @Param("role") String role);
+}
---
+++ Web/Done/donessm/src/main/web/wk/entity/Group.java
@@ -0,0 +1,48 @@
+package wk.entity;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Group {
+
+    @JSONField(name = "groupID")
+    private int groupID;
+    @JSONField(name = "groupName")
+    private String groupName;
+    @JSONField(name = "groupDescribe")
+    private String groupDescribe;
+
+    public int getGroupID() {
+        return groupID;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public String getGroupDescribe() {
+        return groupDescribe;
+    }
+
+    public void setGroupID(int groupID) {
+        this.groupID = groupID;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public void setGroupDescribe(String groupDescribe) {
+        this.groupDescribe = groupDescribe;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "groupID='" + groupID + '\'' +
+                ", groupName='" + groupName + '\'' +
+                ", groupDescribe='" + groupDescribe + '\'' +
+                "}\n";
+    }
+}
